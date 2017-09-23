0abadb40b4dae319fc7c19c6b7dbd840690c624b
fuadajip <fuadajipratomo@gmail.com>
2017-09-23 11:51:51 +0700

    first upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+	res.send('Masterclass Facebook Jakarta - Example Webhook')
+})
+
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'make_indonesian_great_again') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
